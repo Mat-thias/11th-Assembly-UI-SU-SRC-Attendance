20aa3f438d6bb4ef089a2852fc63675618ea2bab
BUSOYE Opegbemi Matthias <96320668+Mat-thias@users.noreply.github.com>
2024-05-05 09:07:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 document.getElementById("attendanceForm").onsubmit = function () {
-    const securityCode = document.getElementById("securityCode").value;
-    const confirmSecurityCode = document.getElementById("confirmSecurityCode").value;
-
-    if (securityCode !== confirmSecurityCode) {
-        alert("Security codes do not match!");
-        return false;
-    }
-
+    
     const email = document.getElementById("email").value;
     if (!validateEmail(email)) {
         alert("Please enter a valid email address!");
         return false;
     }
+    
+    const securityCode = document.getElementById("securityCode").value;
+    const confirmSecurityCode = document.getElementById("confirmSecurityCode").value;
+    if (securityCode !== confirmSecurityCode) {
+        showModal("Security codes do not match!");
+        return false;
+    }
+    // More validations here
 
     const entryChecked = document.getElementById("entry").checked;
     const exitChecked = document.getElementById("exit").checked;
@@ -22,6 +23,25 @@ document.getElementById("attendanceForm").onsubmit = function () {
 
     return true;
 };
+
+function showModal(message) {
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    document.getElementById("modalText").innerText = message;
+    
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 
 function validateEmail(email) {
     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
